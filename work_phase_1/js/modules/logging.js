d9a3c4cee87a989d8f91d7b616af23baa20513e2
Weaverrrr <chunji.wang@uqconnect.edu.au>
2025-04-03 12:43:15 +1000

    week6 sub
    submit in week 6

---
+++ work_phase_1/js/modules/logging.js
@@ -0,0 +1,10 @@
+/**
+ * Module: logging
+ * Generic logging using console.log
+ */
+
+function log(pageName, message) {
+    console.log(`from ${pageName}: ${message}`);
+}
+
+export { logMessage };
